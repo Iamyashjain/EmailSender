61ee53165cb385a303008a559183bb2675a521ba
Iamyashjain <yashjain200502@gmail.com>
2025-04-01 21:34:24 +0530

    added uvicron

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/print/{this}")
+def print_this(this):
+    print(this)
+    return {"this": this}
